c923f92e238bf9da6ac5203a66bdc4d19ec84afe
Phillip Manapat <philmanapat@gmail.com>
2020-04-13 16:43:55 -0400

    updated script

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     });
     // For carousel-begin
     $.ajax({
-        url: "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=f35dc94b7584e054481a5dfa63bfb1c8&format=json",
+        url: "https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=f35dc94b7584e054481a5dfa63bfb1c8&format=json",
         method: "GET"
     }).then(function (trending){
         // console.log(trending);
@@ -42,4 +42,26 @@ $(document).ready(function() {
         }
     });
     // For carousel-end
+
+    //Declare variables
+    var APIKey = "?apikey=bormTRVJ8VGhGmIeOGKrWGP9sMRHoO02";
+    var artistSearch = "/discovery/v2/attractions";
+    var genreSearch = "/discovery/v2/classifications/genres/";
+    var dateSearch= "/discovery/v2/events";
+    var queryUrl = "https://app.ticketmaster.com/" + artistSearch + APIKey;
+    //AJAX call ---- Name of event,artist,link,date and venue
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response){
+        console.log(response)
+        console.log(response._embedded.attractions[0].name);
+        //Loop top 5 responses
+        for (i =0; i < 5; i++){
+            console.log(response._embedded.attractions[i].url);
+            //Append top 5 responses
+            //$("#artist-modal").empty();
+            $("#artist-modal").append("<h1>").text(response._embedded.attractions[i].name);       
+        }
+    })      
 })
